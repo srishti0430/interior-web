4f89f7a5ccf709db55d10dba5cab3b4de5548fd7
srishti0430 <168006172+srishti0430@users.noreply.github.com>
2025-12-05 06:16:12 +0530

    Fix: Final path correction for Logo.png

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/brand/Logo.png";
+import logo from "../../assets/brand/Logo.png";
 import { motion } from "framer-motion";
 
 const NavLinks = [
